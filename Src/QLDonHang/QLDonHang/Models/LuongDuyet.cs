6e0ec24d9e2aba6eefd74cd11e6adacf38d74aa8
caohieeu <caohieeu2003@gmail.com>
2024-12-05 11:25:45 +0700

    commit

---
+++ Src/QLDonHang/QLDonHang/Models/LuongDuyet.cs
@@ -0,0 +1,9 @@
+﻿namespace QLLuongDuyet.Models
+{
+    public class LuongDuyet
+    {
+        public string Id { get; set; }
+        public string TenLuongDuyet { set; get; }   
+        public int ThuTu { set; get; }
+    }
+}
